c75a0412df7da1bd5ac30f154967d74c0dcef3b5
Shubham Mishra <smishra.on.ca@gmail.com>
2024-01-29 01:34:55 -0500

    Added feature that shows intent of the segment and point before user submits left click. Implemented segment and point delete as well.

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -8,45 +8,48 @@ class GraphEditor{
         this.selected = null;
         this.hovered = null;
         this.dragging = false;
+        this.mouse = null;
 
         this.#addEventListeners();
     }
 
     //Private function to add event listeners
     #addEventListeners(){
-        this.canvas.addEventListener("mousedown", (evt) => {
-            if(evt.button == 2){ //right click
-                if(this.hovered){
-                    this.#removePoint(this.hovered);
-                } else {
-                    this.selected = null;
-                
-                }
-            }
-            if(evt.button == 0){ //left click
-            const mouse = new Point(evt.offsetX, evt.offsetY);
-            if(this.hovered){
-                this.#select(this.hovered);
-                this.dragging = true;
-                return;
-            }
-            this.graph.addPoint(mouse);
-            this.#select(mouse);
-            this.hovered = mouse;
-        }
-        });
-
-        this.canvas.addEventListener("mousemove", (evt) => {
-            const mouse = new Point(evt.offsetX, evt.offsetY);
-            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
-            if(this.dragging == true){
-                this.selected.x = mouse.x;
-                this.selected.y = mouse.y;
-            }
-            
-        });
+        this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+        this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
         this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
         this.canvas.addEventListener("mouseup", () => this.dragging = false);
+    }
+
+    #handleMouseMove(evt){
+        this.mouse = new Point(evt.offsetX, evt.offsetY);
+        this.hovered = getNearestPoint(this.mouse, this.graph.points, 10);
+        if(this.dragging == true){
+            this.selected.x = this.mouse.x;
+            this.selected.y = this.mouse.y;
+        }
+        
+    
+    }
+
+    #handleMouseDown(evt){
+        if(evt.button == 2){ //right click
+            if(this.selected){
+                this.selected = null;
+        }else if(this.hovered){
+            this.#removePoint(this.hovered);
+        }
+    }
+        if(evt.button == 0){ //left click
+        if(this.hovered){
+            this.#select(this.hovered);
+            this.dragging = true;
+            return;
+        }
+        this.graph.addPoint(this.mouse);
+        this.#select(this.mouse);
+        this.hovered = this.mouse;
+    }
     }
 
     #select(point){
@@ -74,6 +77,8 @@ class GraphEditor{
         }
         //Draw selected points
         if(this.selected){
+            const intent = this.hovered ? this.hovered : this.mouse;
+            new Segment(this.selected, this.mouse).draw(ctx, {dash : [3,3] });
             this.selected.draw(this.ctx, { outline: true });
         }
     }
--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -15,12 +15,14 @@ class Segment{
     }
 
     //Draws the segment
-    draw(ctx, width = 2, color = "black"){
+    draw(ctx, { width = 2, color = "black", dash = []} = {}){
         ctx.beginPath();
         ctx.lineWidth = width;
         ctx.strokeStyle = color;
+        ctx.setLineDash(dash);
         ctx.moveTo(this.p1.x, this.p1.y);
         ctx.lineTo(this.p2.x, this.p2.y);
         ctx.stroke();
+        ctx.setLineDash([]);
     }
 }
